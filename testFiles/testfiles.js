e2024e9a7bef1661c8d4e057c4c5ff5e899b64b4
shreyas-girjapure <shreyasgirjapure1122@gmail.com>
2024-03-11 23:18:20 +0530

    finally some luck

--- testFiles/testfiles.js
+++ testFiles/testfiles.js
@@ -1,99 +1,77 @@
-import { BufferedTokenStream, CommonToken, ParserRuleContext, RuleContext } from "antlr4ts";
 import {
   ApexLexer,
   CommonTokenStream,
   ApexParser,
   CaseInsensitiveInputStream,
-  MethodCallContext,
+  ParseTreeWalker
 } from "apex-parser";
 
-class VariableDeclarationListener {
-  declaredVariables = {
-    allVariables: [],
-    inputVariables: [],
-    localVariables: [],
-    outputVariables: [],
+class VariableDeclarationListenerTestAnother {
+
+  enterFormalParameter(ctx) {
+    const typeRef = ctx.typeRef();
+    const varName = ctx.id().text;
+    const varType = this.getTypeAsString(typeRef);
+    console.log('Variable Name: ' + varName + ', Variable Type: ' + varType);
   }
-
-  enterEveryRule(ctx) {
-    const ruleIndex = ctx.ruleIndex;
-    const ruleName = parser.ruleNames[ruleIndex];
-    const currentRuleConstructorName = ctx?.ruleContext?.constructor?.name;
-    const currentRuleConstructorValue = ctx?.ruleContext?.start?.text;
-    const parentRuleConstructorName = ctx?.parent?.ruleContext?.constructor?.name;
-    const parentRuleConstructorValue = ctx?.parent?.ruleContext?.start.text;
-    const grandParentRuleConstructorName = ctx?.parent?.parent?.ruleContext?.constructor?.name;
-    const grandParentRuleConstructorValue = ctx?.parent?.parent?.ruleContext?.start.text;
-    const textValue = ctx.ruleContext.start.text;
-    console.log("RULE STARTED " + ruleName)
-    console.log("The value > " + textValue);
-    console.log("The anpther > " + textValue);
-    console.log("The Constructor Name > " + currentRuleConstructorName);
-    console.log("The Constructor Value > " + currentRuleConstructorValue);
-    console.log("The Parent  Name > " + parentRuleConstructorName);
-    console.log("The Parent  Value > " + parentRuleConstructorValue);
-    console.log("The Grand Parent  Name > " + grandParentRuleConstructorName);
-    console.log("The Grand Parent  Value > " + grandParentRuleConstructorValue);
-    console.log("RULE ENDED ")
-    console.log();
-
-  }
-
-
-}
-
-
-class VariableDeclarationListenerTest {
-  declaredVariables = {
-    allVariables: [],
-    inputVariables: [],
-    localVariables: [],
-    outputVariables: [],
-  }
+  getTypeAsString(typeRef) {
+    const typeNames = typeRef.typeName();
   
-
-  enterEveryRule(ctx) {
-    const ruleIndex = ctx.ruleIndex;
-    const ruleName = parser.ruleNames[ruleIndex];
-    const textValue = ctx.start.text;
-    const parentText = ctx?.parent?.ruleContext?.start?.text;
-    const parentRuleIndex = ctx?.parent?.ruleIndex;
-    const parentRuleName = parser.ruleNames[parentRuleIndex];
-    console.log("RULE STARTED " + ruleName +" and index "+ruleIndex);
-    console.log("The value > " + textValue);
-    console.log("Parent Rule Name? > " + parentRuleName + " and index "+parentRuleIndex);
-    console.log("Parent Text? > " + parentText);
-    console.log("RULE ENDED ")
-    console.log();
+    if (typeNames && typeNames.length > 0) {
+      const typeStrings = typeNames.map(typeName => this.getTypeName(typeName));
+      return typeStrings.join('|');
+    }
+  
+    return "";
   }
 
-  exitEveryRule(ctx) {
-    const ruleIndex = ctx.ruleIndex;
-    const ruleName = parser.ruleNames[ruleIndex];
-    const textValue = ctx.start.text;
-    const parentText = ctx?.parent?.ruleContext?.start?.text;
-    const parentRuleIndex = ctx?.parent?.ruleIndex;
 
-    const parentRuleName = parser.ruleNames[ctx?.parent?.ruleIndex];
-    console.log("RULE Exit " + ruleName+" and index "+ruleIndex)
-    console.log("The value > " + textValue);
-    console.log("Parent Rule Name? > " + parentRuleName + " and index "+parentRuleIndex);
-    console.log("Parent Text? > " + parentText);
-    console.log("RULE ENDED ")
-    console.log();
+  getTypeName(typeNameContext) {
+    if (typeNameContext) {
+      const baseType = typeNameContext.id() ? typeNameContext.id().text : "";
+
+      if (typeNameContext.LIST()) {
+        return 'List<' + this.getTypeArguments(typeNameContext.typeArguments()) + '>';
+      } else if (typeNameContext.SET()) {
+        return 'Set<' + this.getTypeArguments(typeNameContext.typeArguments()) + '>';
+      } else if (typeNameContext.MAP()) {
+        return 'Map<' + this.getTypeArguments(typeNameContext.typeArguments()) + '>';
+      } else {
+        return baseType;
+      }
+    }
+
+    return "";
   }
 
+
+  getTypeArguments(typeArgumentsContext) {
+    if (typeArgumentsContext) {
+      const typeList = typeArgumentsContext.typeList();
+      return this.getTypeList(typeList);
+    }
+    return "";
+  }
+
+  getTypeList(typeListContext) {
+    if (typeListContext) {
+      return typeListContext.typeRef().map(typeRef => this.getTypeAsString(typeRef)).join(', ');
+    }
+    return "";
+  }
 }
-// let classBody =
-//   "public class Hello  { public static String sayHello(String xInput, Integer xInteger,List<String> numList){ String anotherVariable = 'default'; Boolean someBool = false; String finalValue = anotherVariable + 'exact'; List<Account> accList = new List<Account>(); return someBool;}}";
 
-
-let classBody = "public class Hello  { public static String sayHello(String xInput){}}";
+let classBody = `public class Hello  { 
+  public static String sayHello(List<Account> accList , String xInput,List<String> someLst , List<List<String>> deepStrings){
+    String xLocal = '1';
+  }
+}`;
 const lexer = new ApexLexer(new CaseInsensitiveInputStream({}, classBody));
 const tokens = new CommonTokenStream(lexer);
-const parser = new ApexParser(tokens);
-lexer.removeErrorListeners();
-parser.removeErrorListeners();
 
-parser.addParseListener(new VariableDeclarationListenerTest());
-let tree = parser.compilationUnit();
+const parser = new ApexParser(tokens)
+
+parser.removeErrorListeners()
+
+const cu = parser.compilationUnit()
+ParseTreeWalker.DEFAULT.walk(new VariableDeclarationListenerTestAnother(), cu)
